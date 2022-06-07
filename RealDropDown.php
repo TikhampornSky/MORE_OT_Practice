3ab2109341137383b282cbd2b51f32da7d7be000
Tikhamporn Tepsut <88883551+TikhampornSky@users.noreply.github.com>
2022-06-07 21:33:00 +0700

    connect to JS in another file

--- RealDropDown.php
+++ RealDropDown.php
@@ -14,7 +14,7 @@
        exit();
     }
   
-    $sql = "select * from person ORDER BY fullname, lastname, id" ;
+    $sql = "select fullname, lastname, id from person ORDER BY fullname, lastname, id" ;
     $result = ($conn->query($sql));
     //declare array to store the data of database
     $row = []; 
@@ -89,8 +89,8 @@
 </div>
 
 <div class="dropdown">
-  <input type="checkbox" id="myCheck" onclick="myFunction()">
-  <label for="myCheck">โปรดเลือกผู้อนุมัติของท่าน</label> 
+    <input type="checkbox" id="myCheck" onclick="myFunction()">
+    <label for="myCheck">โปรดเลือกผู้อนุมัติของท่าน</label> 
     <div id="myDropdown" class="dropdown-content" >
     <input type="text" placeholder="โปรดใส่ชื่อผู้อนุมัติของท่าน" id="myInput" onkeyup="filterFunction()">
     <div id="listTochoose">
@@ -99,7 +99,7 @@
         if(!empty($row))
             foreach($row as $rows) {
         ?>
-            <a onclick="mySelect( '<?php echo $rows['fullname'] . ' ' . $rows['lastname']; ?>' )"> <?php echo $rows['fullname'] . ' ' . $rows['lastname']; ?> <br> <?php echo $rows['id']; ?> </a>
+            <a onclick="mySelect( '<?php echo $rows['fullname'] . ' ' . $rows['lastname']; ?>' , '<?php echo $rows['id']; ?>' )"> <?php echo $rows['fullname'] . ' ' . $rows['lastname']; ?> <br> <?php echo $rows['id']; ?> </a>
         <?php
             }
         ?>
@@ -108,98 +108,7 @@
 </div>
 
 
-<script>
-
-function myFunction() {                                                                                //show dropdown content
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-function filterFunction() {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown");
-  a = div.getElementsByTagName("a");
-  var InputLength = input.value.length ;
-  for (i = 0; i < a.length; i++) {
-    txtValue = a[i].textContent ;
-    var arr = txtValue.split(" ") ;     //['', 'Susan', 'Jeffrey', '', '2147483647', '']
-    var fname = arr[1] ;
-    var lname = arr[2] ;
-    var id = arr[4] ;
-    //console.log(txtValue) ;
-    //console.log(arr) ;
-    //console.log(fname) ;
-    //console.log(lname) ;
-    //console.log(id) ;
-    var isOk = true ;
-    var isOk2 = true ;
-    var isOk3 = true ;
-    var isOk4 = true ;
-    for (j = 0; j < InputLength; j++) {         //search by NAME
-        if (j >= fname.length) {
-            break ;
-        }
-        if (!(fname[j] == input.value[j])) {
-            isOk = false ;
-            break ;
-        }
-    }
-    for (j = 0; j < InputLength; j++) {         //search by LASTNAME
-        if (j >= lname.length) {
-            if (InputLength > lname.length) {
-                isOk2 = false ;
-            }
-            break ;
-        } 
-        if (!(lname[j] == input.value[j])) {
-            isOk2 = false ;
-            break ;
-        }
-    }
-    for (j = 0; j < InputLength; j++) {         ////search by ID
-        if (j >= id.length) {
-            if (InputLength > id.length) {
-                isOk3 = false ;
-            }
-            break ;
-        }
-        if (!(id[j] == input.value[j])) {
-            isOk3 = false ;
-            break ;
-        }
-    }
-    var stringCheck = fname + " " + lname ;
-    if (isOk) {
-        for (j = 0; j < InputLength; j++) {
-            if (j > stringCheck.length) break ;
-            if (!(stringCheck[j] == input.value[j])) {
-                isOk4 = false ;
-                break ;
-            }
-        }
-    }
-
-    if ((isOk && isOk4) || isOk2 || isOk3) {
-        //console.log(input.value + " <match with> " + txtValue) ;
-        if (document.getElementById("listTochoose").style.display == "none") {
-            document.getElementById("listTochoose").style.display = "";
-        }
-        a[i].style.display = "";
-    } else {
-        a[i].style.display = "none";
-    }
-  }
-}
-
-function mySelect(valueSelect) {
-    var arr = valueSelect.split(" ") ;
-    document.getElementById("myInput").value = arr[0] + " " + arr[1] ;
-    console.log(arr[0] + " " + arr[1]) ;
-    document.getElementById("listTochoose").style.display = "none";
-}
-
-</script>
+<script type="text/javascript" src="./dropdownFunction.js"></script>
 
 </body>
 </html>
---
+++ dropdownFunction.js
@@ -0,0 +1,90 @@
+function myFunction() {                                                                                //show dropdown content
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  var InputLength = input.value.length ;
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent ;
+    var arr = txtValue.split(" ") ;     //['', 'Susan', 'Jeffrey', '', '2147483647', '']
+    var fname = arr[1] ;
+    var lname = arr[2] ;
+    var id = arr[4] ;
+    //console.log(txtValue) ;
+    //console.log(arr) ;
+    //console.log(fname) ;
+    //console.log(lname) ;
+    //console.log(id) ;
+    var isOk = true ;
+    var isOk2 = true ;
+    var isOk3 = true ;
+    var isOk4 = true ;
+    for (j = 0; j < InputLength; j++) {         //search by NAME
+        if (j >= fname.length) {
+            break ;
+        }
+        if (!(fname[j] == input.value[j])) {
+            isOk = false ;
+            break ;
+        }
+    }
+    for (j = 0; j < InputLength; j++) {         //search by LASTNAME
+        if (j >= lname.length) {
+            if (InputLength > lname.length) {
+                isOk2 = false ;
+            }
+            break ;
+        } 
+        if (!(lname[j] == input.value[j])) {
+            isOk2 = false ;
+            break ;
+        }
+    }
+    for (j = 0; j < InputLength; j++) {         ////search by ID
+        if (j >= id.length) {
+            if (InputLength > id.length) {
+                isOk3 = false ;
+            }
+            break ;
+        }
+        if (!(id[j] == input.value[j])) {
+            isOk3 = false ;
+            break ;
+        }
+    }
+    var stringCheck = fname + " " + lname ;
+    if (isOk) {
+        for (j = 0; j < InputLength; j++) {
+            if (j > stringCheck.length) break ;
+            if (!(stringCheck[j] == input.value[j])) {
+                isOk4 = false ;
+                break ;
+            }
+        }
+    }
+
+    if ((isOk && isOk4) || isOk2 || isOk3) {
+        //console.log(input.value + " <match with> " + txtValue) ;
+        if (document.getElementById("listTochoose").style.display == "none") {
+            document.getElementById("listTochoose").style.display = "";
+        }
+        a[i].style.display = "";
+    } else {
+        a[i].style.display = "none";
+    }
+  }
+}
+
+function mySelect(valueSelect, id) {
+    var arr = valueSelect.split(" ") ;
+    document.getElementById("myInput").value = arr[0] + " " + arr[1] ;
+    console.log(arr[0] + " " + arr[1]) ;
+    document.getElementById("listTochoose").style.display = "none";
+    var Approve1ID = id ;
+    console.log(Approve1ID) ;
+}
